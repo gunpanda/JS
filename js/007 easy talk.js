a20944e43292006dfcba26464f684701db6deebf
crazylector@gmail.com <Gun8panda>
2022-04-11 19:05:28 +0300

    --

---
+++ js/007 easy talk.js
@@ -0,0 +1,17 @@
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18 лет?", "");
+// console.log(typeof(answer));
+
+// // Все что возвращается от пользоватяеля строка String 
+// при использовании + данные можно привести к числу
+
+const answers = [];
+
+answers[0] = prompt('Как ваше Имя?', "");
+answers[1] = prompt('Как ваша Фамилия?', "");
+answers[2] = prompt('Сколько вам лет? ', "");
+
+console.log(typeof(answers));
+
