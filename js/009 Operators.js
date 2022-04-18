085740d1c149e0cdf985f7d945c99d8f6ba8f402
crazylector@gmail.com <Gun8panda>
2022-04-18 21:58:24 +0300

    Операторы

---
+++ js/009 Operators.js
@@ -0,0 +1,27 @@
+console.log(4 + "5");
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+
+console.log(5 % 2); // возврат остатка от деления 
+
+console.log(2 * 4 === '8'); // == сравнение  === строгое сравнение с учетом типа данных
+// &&    и
+// || или
+// ! обратное значение
+// != не равен
+
+const isChecked = true,
+     isClose = false;
+
+console.log(isChecked || isClose);
+
+
+// Приоритеты выполнения оперторов 
